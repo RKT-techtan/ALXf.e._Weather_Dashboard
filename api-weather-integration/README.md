794aa074870f0539e58095ba0bf02d655f9d69e2
RKT-techtan <fandfilian098@gmail.com>
2025-03-21 18:53:43 +0300

    Added a few lines of code to fix the wmainpage

--- api-weather-integration/README.md
+++ api-weather-integration/README.md
@@ -1,3 +1,4 @@
 FE Capstone Project: Weather Dashboard
 
 1. Added content to README.md, tailwind.config.js, index.css
+2. Worked on and updated the MAinpage file tah is to be the mainpage of the weather APP.
--- api-weather-integration/src/components/Mainpage.jsx
+++ api-weather-integration/src/components/Mainpage.jsx
@@ -4,15 +4,15 @@ import { Component } from "react";
 
 function Mainpage() {
     return (
-        <div className="bg-slate-500">
+        <div className=" mt-0 ">
 
-            <div className="bg-slate-400 rounded-2xl h-96 ">
-            <h1 className="text-black text-6xl ">Weather sheet</h1>
+            <div className="bg-slate-400 w-5s rounded-2xl h-96 ">
+            <h1 className="text-black text-6xl text-7xl ">Weather sheet</h1>
             <div>
-            <button className=" bg-red-500 w-40 text-align position: relative top-2 ">Log in</button>
+            <button className=" bg-red-500 top-11 w-40 text-align relative top-2 ">Log In</button>
             </div>
             <div>
-            <button className="bg-red-500 w-40 position: relative top-3.5 ">Sign in</button>
+            <button className="bg-red-500 w-40 top-12 relative top-3.5 ">Sign In</button>
             </div>
         <footer></footer>
         </div>
